15ea8b691cc5784230b5058910b4fb8b3b90b148
LuckyJoker <1046944896@qq.com>
2020-04-30 11:03:37 +0800

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #![no_std] //不链接 Rust 标准库
 #![no_main] // 禁用所有 Rust 层级的入口点
 #![feature(asm)] // 使用内联汇编
+mod vga_buffer;
 use core::panic::PanicInfo;
-
 /// 这个函数将在 panic 时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -15,12 +16,7 @@ static HELLO: &[u8] = b"Hello World";
 pub extern "C" fn _start() -> ! {
     // 因为编译器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
     // 默认命名为 `_start`
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
