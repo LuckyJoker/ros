e2ce0779b91f43ccd35281126ee385ee2dd174f9
LuckyJoker <1046944896@qq.com>
2020-09-01 10:41:54 +0800

    Testing

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
-#![no_std] //不链接 Rust 标准库
-#![no_main] // 禁用所有 Rust 层级的入口点
-#![feature(asm)] // 使用内联汇编
-mod vga_buffer;
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(ros::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
-/// 这个函数将在 panic 时被调用
+use ros::println;
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World";
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    ros::test_panic_handler(info)
+}
 
-#[no_mangle] // 不重整函数名
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // 因为编译器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
-    // 默认命名为 `_start`
     println!("Hello World{}", "!");
-    panic!("Some panic message");
-    loop {}
+    #[cfg(test)]
+    test_main();
+    loop{}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
